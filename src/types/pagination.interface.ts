ac7ee7af58e0f74cb28ec0f237ade726e551b8d7
Andrii Chertkov <andriichertkov@gmail.com>
2023-05-25 15:39:39 +0300

    fix

--- src/types/pagination.interface.ts
+++ src/types/pagination.interface.ts
@@ -1,19 +1,3 @@
-// export interface QueryPagination {
-//   condition?: string;
-//   sortBy?: string;
-//   sortDirection?: 'asc' | 'desc';
-//   pageNumber?: string;
-//   pageSize?: string;
-// }
-
-// export interface Pagination {
-//   condition: string | null;
-//   sortBy: string;
-//   sortDirection: 'asc' | 'desc';
-//   page: number;
-//   pageSize: number;
-// }
-
 export interface WithPagination<T> {
   pagesCount: number;
   page: number;
