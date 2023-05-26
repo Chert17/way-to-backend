e7ce220b2e515b10a532a023ab045cf36da67f09
Andrii Chertkov <andriichertkov@gmail.com>
2023-05-26 09:34:30 +0300

    add comments & users to testing all-data

--- src/modules/delete-all/delete-all.controller.ts
+++ src/modules/delete-all/delete-all.controller.ts
@@ -4,13 +4,17 @@ import { Controller, Delete, HttpCode } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 
 import { Blog } from '../blogs/blogs.schema';
+import { Comment } from '../comments/comments.schema';
 import { Post } from '../posts/posts.schema';
+import { User } from '../users/users.schema';
 
 @Controller('/testing/all-data')
 export class DeleteAllController {
   constructor(
     @InjectModel(Blog.name) private blogModel: Model<Blog>,
     @InjectModel(Post.name) private postModel: Model<Post>,
+    @InjectModel(Comment.name) private commentModel: Model<Comment>,
+    @InjectModel(User.name) private userModel: Model<User>,
   ) {}
 
   @Delete()
@@ -18,5 +22,7 @@ export class DeleteAllController {
   async delteAll() {
     await this.blogModel.deleteMany();
     await this.postModel.deleteMany();
+    await this.commentModel.deleteMany();
+    await this.userModel.deleteMany();
   }
 }
