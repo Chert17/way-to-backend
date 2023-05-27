1340f01ff4344af65afa1b564c5aa4c6c05c9540
Andrii Chertkov <andriichertkov@gmail.com>
2023-05-27 13:36:43 +0300

    add users body validation

--- src/modules/users/dto/input/create.user.dto.ts
+++ src/modules/users/dto/input/create.user.dto.ts
@@ -1,13 +1,22 @@
-import { IsEmail, IsString } from 'class-validator';
+import { Transform } from 'class-transformer';
+import { IsEmail, IsString, Length, Matches } from 'class-validator';
 
 export class CreateUserDto {
   @IsString()
+  @Length(3, 10)
+  @Transform(({ value }) => value.trim())
   readonly login: string;
 
   @IsString()
-  @IsEmail()
-  readonly email: string;
+  @Length(6, 20)
+  @Transform(({ value }) => value.trim())
+  readonly password: string;
 
   @IsString()
-  readonly password: string;
+  @IsEmail()
+  @Matches(
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+  )
+  @Transform(({ value }) => value.trim())
+  readonly email: string;
 }
