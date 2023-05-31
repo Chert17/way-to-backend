dad5e59c0eaef7e67c8308f58cf1d9d94545a8d9
Andrii Chertkov <andriichertkov@gmail.com>
2023-05-31 12:16:33 +0300

    fix confirm register endpoint

--- src/infra/decorators/auth/confirm.code.exist.ts
+++ src/infra/decorators/auth/confirm.code.exist.ts
@@ -10,13 +10,13 @@ export class ConfirmCodeExist implements ValidatorConstraintInterface {
   constructor(private usersRepo: UsersRepo) {}
 
   async validate(value: string) {
-    const confirmEmail = await this.usersRepo.getConfirmEmailByCode(value);
+    const emailInfo = await this.usersRepo.getConfirmEmailByCode(value);
 
-    if (!confirmEmail) return false;
+    if (!emailInfo) return false;
 
-    if (confirmEmail.isConfirmed === true) return false; // user has already been verified
+    if (emailInfo.isConfirmed === true) return false; // user has already been verified
 
-    if (confirmEmail.expirationDate < new Date()) return false; // invalid expirationDate
+    if (emailInfo.expirationDate < new Date()) return false; // invalid expirationDate
 
     return true;
   }
