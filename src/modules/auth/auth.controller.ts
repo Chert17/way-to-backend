11591b65471ec12128de15a2fe61de70d38e2cbb
Andrii Chertkov <andriichertkov@gmail.com>
2023-05-31 11:19:32 +0300

    fix login endpoint #2

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -87,7 +87,6 @@ export class AuthController {
   @HttpCode(HttpStatus.NO_CONTENT)
   async newPassword() {}
 
-
   private _setRefreshTokenToCookie(res: Response, refreshToken: string) {
     return res.cookie('refreshToken ', refreshToken, {
       httpOnly: Boolean(this.configService.get(COOKIE_HTTP_ONLY)),
