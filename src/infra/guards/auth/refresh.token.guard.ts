d96fdc39d9972bfd22036bbb0ba99944e313b3ec
Andrii Chertkov <andriichertkov@gmail.com>
2023-06-04 12:44:06 +0300

    remove check lastActiveDate in refresh token guard

--- src/infra/guards/auth/refresh.token.guard.ts
+++ src/infra/guards/auth/refresh.token.guard.ts
@@ -38,10 +38,6 @@ export class RefreshTokenGuard implements CanActivate {
 
     if (!device) throw new UnauthorizedException(); // not that kind of device in DB
 
-    const iatToken = this.jwtService.getTokenIat(refreshToken);
-
-    if (device.lastActiveDate !== iatToken) throw new UnauthorizedException();
-
     request.refreshTokenPayload = payload;
 
     return true;
