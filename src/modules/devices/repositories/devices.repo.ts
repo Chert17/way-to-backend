6c8076589c1304244f6d88163b451ba86cf44bc9
Andrii Chertkov <andriichertkov@gmail.com>
2023-06-04 12:30:01 +0300

    add check lastActiveDate in refresh token guard

--- src/modules/devices/repositories/devices.repo.ts
+++ src/modules/devices/repositories/devices.repo.ts
@@ -42,11 +42,11 @@ export class DevicesRepo {
     return await this.devicesModel.deleteOne({ userId, deviceId });
   }
 
-  async checkDevice(userId: string, deviceId: string): Promise<boolean> {
+  async checkDevice(userId: string, deviceId: string) {
     const result = await this.devicesModel.findOne({ userId, deviceId });
 
     if (!result) return false;
 
-    return !!result;
+    return result;
   }
 }
