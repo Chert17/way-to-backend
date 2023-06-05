a87e92252fccd257b2c483c646620e87a8b9e263
Andrii Chertkov <andriichertkov@gmail.com>
2023-06-05 19:58:54 +0300

    add ban info to user view model

--- src/modules/users/dto/view/user.view.dto.ts
+++ src/modules/users/dto/view/user.view.dto.ts
@@ -3,4 +3,9 @@ export class UserViewDto {
   readonly login: string;
   readonly email: string;
   readonly createdAt: string;
+  readonly banInfo: {
+    isBanned: boolean;
+    banDate: string | null;
+    banReason: string;
+  };
 }
