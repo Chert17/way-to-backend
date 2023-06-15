0f44c0ca1b2be6bf4faf7773e70e22460819a483
Andrii Chertkov <112465164+Chert17@users.noreply.github.com>
2023-06-15 17:40:37 +0300

    Hw 17 (#91)
    * init hw-17
    
    * add create user endpoint
    
    * refactor create user by sa & add tests
    
    * refactor user relations & create user by sa
    
    * refactor user relations & create user by sa
    
    * migration create user method to use-case
    
    * add ban and unban user logic & tests
    
    * fix spaces
    
    * fix enumtables names in user query repo

--- src/modules/users/repositories/users.query.repo.ts
+++ src/modules/users/repositories/users.query.repo.ts
@@ -3,7 +3,10 @@ import { DataSource } from 'typeorm';
 import { Injectable } from '@nestjs/common';
 import { InjectDataSource } from '@nestjs/typeorm';
 
+import { UsersSqlTables } from '../../../utils/tables/users.sql.tables';
 import { UserViewDto } from '../dto/user.view.dto';
+
+const { USERS_TABLE } = UsersSqlTables;
 
 @Injectable()
 export class UsersQueryRepo {
@@ -12,7 +15,7 @@ export class UsersQueryRepo {
   async getUserById(userId: string): Promise<UserViewDto> {
     const user = await this.dataSource.query(
       `SELECT id, login, email, created_at AS "createdAt"
-      FROM users u
+      FROM ${USERS_TABLE} u
       WHERE u.id = '${userId}'`,
     );
 
