cc6743fad2e0ddf87b7e2deea8eeb17723b4035b
Andrii Chertkov <andriichertkov@gmail.com>
2023-07-10 11:36:48 +0300

    add send confirm code for auth telegram bot

--- src/modules/telegram/telegram.controller.ts
+++ src/modules/telegram/telegram.controller.ts
@@ -7,12 +7,18 @@ import {
   Post,
   UseGuards,
 } from '@nestjs/common';
+import { CommandBus } from '@nestjs/cqrs';
 
+import { ReqUser } from '../../infra/decorators/params/req.user.decorator';
 import { JwtAuthGuard } from '../../infra/guards/jwt.auth.guard';
+import { User } from '../users/entities/user.entity';
 import { TelegramUpdateMessage } from './dto/update.message.dto';
+import { AuthTelegramBotCommand } from './use-case/auth.bot.use-case';
 
 @Controller('integrations/telegram')
 export class TelegramController {
+  constructor(private readonly commandBus: CommandBus) {}
+
   @Post('/webhook')
   @HttpCode(HttpStatus.NO_CONTENT)
   connect(@Body() payload: TelegramUpdateMessage) {
@@ -23,5 +29,7 @@ export class TelegramController {
 
   @Get('/auth-bot-link')
   @UseGuards(JwtAuthGuard)
-  getLink() {}
+  getLink(@ReqUser() user: User) {
+    return this.commandBus.execute(new AuthTelegramBotCommand(user.id));
+  }
 }
